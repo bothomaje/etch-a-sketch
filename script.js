0e980a1c818c46e4a9f1c2a0895cae261da3f2ba
bothomaje <bothomaje@gmail.com>
2026-06-06 10:54:11 +0200

    Feat: More colours
    Add functionality to paint the grid in randomly selected colours instead of only black

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const gridContainer = document.querySelector(".container");
 const button = document.querySelector(".num-squares");
 
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
 function createRow(size) {
     const row = document.createElement("div");
     row.classList.add("row");
@@ -10,7 +18,7 @@ function createRow(size) {
         cell.classList.add("cell");
 
         cell.addEventListener("mouseenter", (e) => {
-            e.target.style.backgroundColor = "black";
+            e.target.style.backgroundColor = getRandomColor();
         });
 
         row.appendChild(cell);
